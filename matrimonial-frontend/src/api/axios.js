485b9bc1a1a06f64a06392bd3f39c2d23739f539
Rounak Bhuiya <rounakbhuiya@gmail.com>
2025-10-10 13:13:34 +0530

    message 2

--- matrimonial-frontend/src/api/axios.js
+++ matrimonial-frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://couplemarriage.com/api',
+  baseURL: import.meta.env.VITE_API_URL || 'https://www.couplemarriage.com/api',
   headers: { 'Accept': 'application/json' },
    withCredentials: true,
 });
