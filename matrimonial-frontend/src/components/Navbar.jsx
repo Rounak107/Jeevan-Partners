5948be29259becd04886e51a839ad715757b4a20
Rounak Bhuiya <rounakbhuiya@gmail.com>
2025-10-13 13:06:35 +0530

    navbar 6

--- matrimonial-frontend/src/components/Navbar.jsx
+++ matrimonial-frontend/src/components/Navbar.jsx
@@ -93,7 +93,7 @@ useEffect(() => {
       <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
     </svg>
   </div>
-  <span className="bg-gradient-to-r from-rose-500 to-pink-600 bg-clip-text text-transparent">
+  <span className="bg-gradient-to-r from-Violet-500 to-pink-600 bg-clip-text text-transparent">
     Couple Marriage
   </span>
 </Link>
