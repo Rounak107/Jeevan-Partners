d6d56538cb06465a69906cad7e7d6b5e06bce43a
Rounak Bhuiya <rounakbhuiya@gmail.com>
2025-10-13 12:52:30 +0530

    navbar 4

--- matrimonial-frontend/src/components/Navbar.jsx
+++ matrimonial-frontend/src/components/Navbar.jsx
@@ -62,7 +62,7 @@ useEffect(() => {
 
   return (
     <>
-      <header className="fixed top-0 left-0 right-0 z-50 bg-gradient-to-r from-purple-600 via-pink-600 to-violet-700">
+      <header className="fixed top-0 left-0 right-0 z-50 bg-gradient-to-r from-yellow-400 via-amber-500 to-orange-600">
         <div className="container mx-auto px-6 py-4">
           <div className="flex items-center justify-between">
             {/* Logo */}
